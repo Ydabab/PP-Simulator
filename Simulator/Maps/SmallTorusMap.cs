ce20128cfcbbbd5d712c3cb3b9ad4b0e45ff136f
Ydabab <krysti248@gmail.com>
2024-11-16 20:26:34 +0100

    TDD

---
+++ Simulator/Maps/SmallTorusMap.cs
@@ -0,0 +1,46 @@
+﻿namespace Simulator.Maps;
+
+public class SmallTorusMap : Map
+{
+    public int Size { get; }
+    private Rectangle boundaries;
+
+    public SmallTorusMap(int size)
+    {
+        if (size < 5 || size > 20)
+            throw new ArgumentOutOfRangeException(nameof(size), "Rozmiar mapy musi być pomiędzy 5 a 20.");
+
+        Size = size;
+        boundaries = new Rectangle(0, 0, Size - 1, Size - 1);
+    }
+    public override bool Exist(Point p)
+    {
+        return boundaries.Contains(p);
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        Point nextPoint = p.Next(d);
+        return d switch
+        {
+            Direction.Up => Exist(nextPoint) ? nextPoint : new Point(p.X, 0),
+            Direction.Right => Exist(nextPoint) ? nextPoint : new Point(0, p.Y),
+            Direction.Down => Exist(nextPoint) ? nextPoint : new Point(p.X, Size - 1),
+            Direction.Left => Exist(nextPoint) ? nextPoint : new Point(Size - 1, p.Y),
+            _ => default,
+        };
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        Point nextPoint = p.NextDiagonal(d);
+        return d switch
+        {
+            Direction.Up => Exist(nextPoint) ? nextPoint : new Point((p.X + 1) % Size, (p.Y + 1) % Size),
+            Direction.Right => Exist(nextPoint) ? nextPoint : new Point((p.X + 1) % Size, (p.Y - 1 + Size) % Size),
+            Direction.Down => Exist(nextPoint) ? nextPoint : new Point((p.X - 1 + Size) % Size, (p.Y - 1 + Size) % Size),
+            Direction.Left => Exist(nextPoint) ? nextPoint : new Point((p.X - 1 + Size) % Size, (p.Y + 1) % Size),
+            _ => default,
+        };
+    }
+}
