1f5f6803bdcf556b4f5532969f9fc8ae9c9f9a38
Ydabab <krysti248@gmail.com>
2024-12-18 00:33:41 +0100

    SimWeb FIX

---
+++ WebSimulator/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace WebApplication1.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+    public void OnPost()
+    {
+
+    }
+}
